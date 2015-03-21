a859e248477527251569de52105659242a4cf1ef
Edgar Pabon <edgar.pabon@gmail.com>
2015-03-21 01:59:37 -0600

    Added ability to store temporary UI state

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,20 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -22,6 +35,10 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
